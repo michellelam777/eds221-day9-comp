7fddcc6410720fabeba31837ceb57e853607d306
michellelam777 <michellelam777@gmail.com>
2022-08-18 14:23:27 -0700

    practice with reprex

--- reprex_practice.R
+++ reprex_practice.R
@@ -1,1 +1,37 @@
 # Practicing reprex
+
+### NOT A REPREX
+library(tidyverse)
+library(palmerpenguins)
+
+penguins |>
+  select(species, body_mass_g, flipper_length_mm, year) |>
+  filter(species == "Chinstrap") |>
+  str_to_lower(species) |>
+  group_by(island) |>
+  summarize(mean(body_mass_g, na.rm = TRUE),
+            mean(flipper_length_mm, na.rm = TRUE))
+
+### A REPREX
+library(tidyverse)
+
+warpbreaks |>
+  str_to_lower(wool)
+
+### A FIXING REPREX
+library(tidyverse)
+
+warpbreaks |>
+  mutate(wool = str_to_lower(wool))
+
+### A REPREX WITH A SYNTHESIZED DATA FRAME
+library(tidyverse)
+
+example_df <- tribble(
+  ~type, ~color,
+  "Pants", "blue",
+  "Shoe", "pink"
+)
+
+example_df |>
+  mutate(type = str_to_lower(type))
